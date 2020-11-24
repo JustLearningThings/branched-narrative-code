06deb24891b6c353b30db616a29944a25335f3cc
Denny06 <denismacio@outlook.com>
2020-11-24 21:27:41 +0200

    Updates

---
+++ precache-manifest.dd79674d6e4f22cdc48f7ac5c2847085.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea3a67292a20658feb9374e124bfbe9f",
+    "url": "/branched-narrative-code/index.html"
+  },
+  {
+    "revision": "0b28de848b31388feef4",
+    "url": "/branched-narrative-code/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "66c4cc73152d605352bf",
+    "url": "/branched-narrative-code/static/js/2.be540787.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/branched-narrative-code/static/js/2.be540787.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b28de848b31388feef4",
+    "url": "/branched-narrative-code/static/js/main.ff36a4ed.chunk.js"
+  },
+  {
+    "revision": "db87987b1c4ee51b77c3",
+    "url": "/branched-narrative-code/static/js/runtime-main.4c6f91d5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/branched-narrative-code/static/media/logo.5d5d9eef.svg"
+  }
+]);
